5b7adec2146c8abbb68e6a1665575a2eb55758df
Maciej Stachurski <ext-maciej.stachurski@here.com>
2021-10-14 13:35:48 +0200

    Fixed after review comments.

--- src/main/resources/create-app-topic.sh
+++ src/main/resources/create-app-topic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic btcusd-transactions --replication-factor 2 --partitions 1
+/usr/lib/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic btcusd-transactions --replication-factor 3 --partitions 1
